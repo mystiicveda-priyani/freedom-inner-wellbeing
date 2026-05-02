6940fb51400b5a2b40b6b6db562b3c17406534c7
Priyanka Pungaonkar <priyanka.pungaonkar95@gmail.com>
2026-05-02 09:29:59 +0530

    fixed issue

--- src/components/home/AboutPreview.tsx
+++ src/components/home/AboutPreview.tsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion";
 import Button from "@/components/ui/Button";
 import Card from "@/components/ui/Card";
 import Container from "@/components/ui/Container";
-import SectionHeading from "@/components/ui/SectionHeading";
 import { EXTERNAL_LINKS } from "@/lib/constants";
 
 export default function AboutPreview() {
